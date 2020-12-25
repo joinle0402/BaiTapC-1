618e0f9b3ca796ee995e8ec080fcb6bc1e54a2c6
joinle0402 <nhoxhero073@gmail.com>
2020-12-25 20:32:58 +0700

    BaiTap 1 Hoan Chinh

--- BaiTap1/Program.cs
+++ BaiTap1/Program.cs
@@ -2,7 +2,7 @@
 
 namespace BaiTap1
 {
-	Class CongNhan
+	class CongNhan
 	{
 		private string soCMND;
 		private string hoTen;
@@ -13,71 +13,70 @@ namespace BaiTap1
 		public string SoCMND
 		{
 			get { return soCMND; }
-			set { }
+			set { soCMND = value; }
 		}
-
 
 		public CongNhan()
 		{
-			Console.Write(“Nhap SoCMND =  ”);
-			soCMND = Console.WriteLine();
+			Console.Write("Nhap SoCMND =  ");
+			soCMND = Console.ReadLine();
 
-			Console.Write(“Nhap  Ho Ten = ”);
-			hoTen = Console.WriteLine();
+			Console.Write("Nhap  Ho Ten = ");
+			hoTen = Console.ReadLine();
 
 			do
 			{
-				Console.Write(“Nhap Gioi Tinh(1.Nu | 2.Nam) = ”);
-				gioiTinh = Convert.ToInt32(Console.WriteLine());
+				Console.Write("Nhap Gioi Tinh(1.Nu | 2.Nam) = ");
+				gioiTinh = Convert.ToInt32(Console.ReadLine());
 
 				if (gioiTinh != 1 && gioiTinh != 2)
 				{
-					Console.WriteLine(“Khong ton tai gioi tinh nay ?”);
+					Console.WriteLine("Khong ton tai gioi tinh nay ?");
 				}
 			}
 			while (gioiTinh != 1 && gioiTinh != 2);
 
-			Console.Write(“Nhap ngay sinh = ”);
-			ngaySinh = Convert.ToDateTime(Console.WriteLine());
+			Console.Write("Nhap ngay sinh = ");
+			ngaySinh = Convert.ToDateTime(Console.ReadLine());
 
-			Console.Write(“Nhap Dia chi =  ”);
-			diaChi = Console.WriteLine();
+			Console.Write("Nhap Dia chi =  ");
+			diaChi = Console.ReadLine();
 		}
 
 	public virtual void Xuat()
 	{
-		String gioiTinhCongNhan = “”;
+		string gioiTinhCongNhan = "";
 
-		If(gioiTinh == 1)  gioiTinhCongNhan = “Nu”;
-		Else if (gioiTinh == 2) gioiTinhCongNhan = “Nam”;
+		if (gioiTinh == 1)  gioiTinhCongNhan = "Nu";
+		else if (gioiTinh == 2) gioiTinhCongNhan = "Nam";
 
-		Console.Write("(SoCMND = { 0}, HoTen = { 1}, GioiTinh = { 2}, NgaySinh = {3}, DiaChi = {4})",  soCMND, hoTen, gioiTinhCongNhan, ngaySinh.ToString(“dd / mm / yyyy”), diaChi);
+		Console.Write("(SoCMND = {0}, HoTen = {1}, GioiTinh = {2}, NgaySinh = {3}, DiaChi = {4})",  soCMND, hoTen, gioiTinhCongNhan, ngaySinh.ToString("dd/mm/yyyy"), diaChi);
 	}
 
 }
 
-Class CongNhanSX : CongNhan
+class CongNhanSX : CongNhan
 {
-	Private string maCongNhan;
-	Private string maDonVi;
-	Private int chucVu;
+	private string maCongNhan;
+	private string maDonVi;
+	private int chucVu;
 
 	public CongNhanSX() : base()
 	{
-		Console.Write(“Nhap Ma Cong Nhan =  ”);
-		maCongNhan = Console.WriteLine();
+		Console.Write("Nhap Ma Cong Nhan =  ");
+		maCongNhan = Console.ReadLine();
 
-		Console.Write(“Nhap Ma Don Vi =  ”);
-		maDonVi = Console.WriteLine();
+		Console.Write("Nhap Ma Don Vi = ");
+		maDonVi = Console.ReadLine();
 
 		do
 		{
-			Console.Write(“Nhap Chuc Vu(1.Khong To Truong | 2.To Truong) =  ”);
-			chucVu = Convert.ToInt32(Console.WriteLine());
+			Console.Write("Nhap Chuc Vu(1.Khong To Truong | 2.To Truong) =  ");
+			chucVu = Convert.ToInt32(Console.ReadLine());
 
 			if (chucVu != 1 && chucVu != 2)
 			{
-				Console.WriteLine(“Khong ton tai chuc vu  nay ?”);
+				Console.WriteLine("Khong ton tai chuc vu  nay ?");
 			}
 		}
 		while (chucVu != 1 && chucVu != 2);
@@ -85,77 +84,90 @@ Class CongNhanSX : CongNhan
 
 	public override void Xuat()
 	{
-		Base.Xuat();
-		String chucVuCongNhan = “”;
+		base.Xuat();
+		String chucVuCongNhan = "";
 
-		If(chucVu == 1)  chucVuCongNhan = “Khong To Truong”;
-		Else if (chucVu == 2) chucVuCongNhan = “To Truong”;
+		if(chucVu == 1)  chucVuCongNhan = "Khong To Truong";
+		else if (chucVu == 2) chucVuCongNhan = "To Truong";
 
-		Console.Write( “(MaCongNhan = { 0}, MaDonVi = { 1}, ChucVu = { 2})”, maCongNhan, maDonVi, chucVuCongNhan );
-}
+		Console.Write("(MaCongNhan = { 0}, MaDonVi = { 1}, ChucVu = { 2})", maCongNhan, maDonVi, chucVuCongNhan );
+	
+	}
 
-public static bool operator ==(CongNhanSX cn1, CongNhanSX cn2)
-{
-	Return cn1.maCongNhan == cn2.maCongNhan;
-}
+	public static bool operator ==(CongNhanSX cn1, CongNhanSX cn2)
+	{
+		return cn1.maCongNhan == cn2.maCongNhan;
+	}
 
-// true false
-public static bool operator !=(CongNhanSX cn1, CongNhanSX cn2)
-{
-	Return cn1.maCongNhan != cn2.maCongNhan;
-}
+	public static bool operator !=(CongNhanSX cn1, CongNhanSX cn2)
+	{
+		return cn1.maCongNhan != cn2.maCongNhan;
+	}
 
 }
 
 	class Program
-{
-	Private CongNhanSX[] ChuyenSX;
-		Private int n;
-
-	Static void Main()
 	{
-		Do
-{
-			Try
-	{
-				Console.Write(“Nhap n =  ”);
-				n = Convert.ToInt32(Console.WriteLine());
-			}
-			Catch(FormatException e)
-{
-				Console.WriteLine(“Nhap khong dung kieu du lieu! Nhap lai!”)
-	Continue;
-			}
+		private static CongNhanSX[] ChuyenSX;
+		private static int n;
 
-			If(n < 0)
-{
-				Console.WriteLine(“Nhap ko hop le! N PHAI la so Duong”);
-			}
-		} while (n < 0) ;
-
-		ChuyenSX = new CongNhanSX[n];
-
-		NhapDanhSach();
-
-		XuatDanhSach();
-
-		Console.ReadLine();
-	}
-
-	Static void NhapDanhSach()
-	{
-		For(int i = 0; I < n; i++)
-{
-			ChuyenSX[i] = new CongNhanSX();
-		}
-	}
-
-	Static void XuatDanhSach()
-	{
-		For(int i = 0; I < n; i++)
+		static void Main()
 		{
-			ChuyenSX[i].Xuat();
+			do
+			{
+				try
+				{
+					Console.Write("Nhap n =  ");
+					n = Convert.ToInt32(Console.ReadLine());
+				}
+				catch(FormatException e)
+				{
+						Console.WriteLine("Nhap khong dung kieu du lieu! Nhap lai!");
+						continue;
+				}
+
+				if(n < 0)
+				{
+					Console.WriteLine("Nhap ko hop le! N PHAI la so Duong");
+				}
+			} while (n < 0) ;
+
+			ChuyenSX = new CongNhanSX[n];
+
+			NhapDanhSach();
+
+			XuatDanhSach();
+
+			Console.ReadLine();
 		}
-	}
-} 
+
+		static void NhapDanhSach()
+		{
+			for(int i = 0; i < n; i++)
+			{
+				ChuyenSX[i] = new CongNhanSX();
+			}
+		}
+
+		static void XuatDanhSach()
+		{
+			for (int i = 0; i < n; i++)
+			{
+				ChuyenSX[i].Xuat();
+			}
+		}
+
+		static bool KiemTraTonTaiCongNhan(CongNhanSX congNhanKiemTra)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (congNhanKiemTra == ChuyenSX[i])
+                {				
+					return true;
+				}
+            }
+
+			return false;
+        }
+	} 
 }
